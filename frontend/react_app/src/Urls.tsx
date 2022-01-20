37deb3aeec589eaec64b0f0cbaadf3c41833da52
Lukasz Blachnicki <lblachnicki@gmail.com>
2022-01-20 20:47:10 +0100

    Configure routes

--- frontend/react_app/src/Urls.tsx
+++ frontend/react_app/src/Urls.tsx
@@ -1,19 +1,26 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, {Component, useEffect, useState} from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Dashboard from "./features/dashboard/Dashboard";
 
 function Urls(props: JSX.IntrinsicAttributes) {
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+    useEffect(() => {
+        setIsLoggedIn(false)
+    }, [])
 
     return (
         <div>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/login" element={<Login/>}/>
+                    <Route path="/login" element={<Login/>} />
                     <Route path="/register" element={<Register/>}/>
-                    <Route path="/" element={<Dashboard/>}/>
+                    <Route path="/dashboard" element={isLoggedIn ? <Dashboard/>: <Navigate to="/login" />}/>
+
+                    <Route path="/" element={isLoggedIn ? <Dashboard /> : <Navigate to="/login" />} />
                 </Routes>
             </BrowserRouter>
         </div>
--- frontend/react_app/src/features/dashboard/Dashboard.tsx
+++ frontend/react_app/src/features/dashboard/Dashboard.tsx
@@ -46,62 +46,19 @@ const AppBar = styled(MuiAppBar, {
   }),
 }));
 
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-  ({ theme, open }) => ({
-    '& .MuiDrawer-paper': {
-      position: 'relative',
-      whiteSpace: 'nowrap',
-      width: drawerWidth,
-      transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-      boxSizing: 'border-box',
-      ...(!open && {
-        overflowX: 'hidden',
-        transition: theme.transitions.create('width', {
-          easing: theme.transitions.easing.sharp,
-          duration: theme.transitions.duration.leavingScreen,
-        }),
-        width: theme.spacing(7),
-        [theme.breakpoints.up('sm')]: {
-          width: theme.spacing(9),
-        },
-      }),
-    },
-  }),
-);
-
 const mdTheme = createTheme();
 
 function DashboardContent() {
-  const [open, setOpen] = React.useState(true);
-  const toggleDrawer = () => {
-    setOpen(!open);
-  };
-
   return (
     <ThemeProvider theme={mdTheme}>
       <Box sx={{ display: 'flex' }}>
         <CssBaseline />
-        <AppBar position="absolute" open={open}>
+        <AppBar position="absolute">
           <Toolbar
             sx={{
               pr: '24px', // keep right padding when drawer closed
             }}
           >
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              onClick={toggleDrawer}
-              sx={{
-                marginRight: '36px',
-                ...(open && { display: 'none' }),
-              }}
-            >
-              <MenuIcon />
-            </IconButton>
             <Typography
               component="h1"
               variant="h6"
@@ -109,33 +66,13 @@ function DashboardContent() {
               noWrap
               sx={{ flexGrow: 1 }}
             >
-              Dashboard
+              Budgets
             </Typography>
             <IconButton color="inherit">
-              <Badge badgeContent={4} color="secondary">
-                <NotificationsIcon />
-              </Badge>
+              Logout
             </IconButton>
           </Toolbar>
         </AppBar>
-        <Drawer variant="permanent" open={open}>
-          <Toolbar
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'flex-end',
-              px: [1],
-            }}
-          >
-            <IconButton onClick={toggleDrawer}>
-              <ChevronLeftIcon />
-            </IconButton>
-          </Toolbar>
-          <Divider />
-          <List>{mainListItems}</List>
-          <Divider />
-          <List>{secondaryListItems}</List>
-        </Drawer>
         <Box
           component="main"
           sx={{
